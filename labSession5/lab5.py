9e67dfa5ff664bbe3c863b711fc7937140ec9b5e
DiegoBielsa <776564@unizar.es>
2023-12-02 23:34:35 +0100

    Lab5: full ended (without optional)

--- labSession5/lab5.py
+++ labSession5/lab5.py
@@ -10,6 +10,34 @@ import scipy.optimize as scOptim
 import scipy.io as sio
 import math as math
 
+def draw3DLine(ax, xIni, xEnd, strStyle, lColor, lWidth):
+    """
+    Draw a segment in a 3D plot
+    -input:
+        ax: axis handle
+        xIni: Initial 3D point.
+        xEnd: Final 3D point.
+        strStyle: Line style.
+        lColor: Line color.
+        lWidth: Line width.
+    """
+    ax.plot([np.squeeze(xIni[0]), np.squeeze(xEnd[0])], [np.squeeze(xIni[1]), np.squeeze(xEnd[1])], [np.squeeze(xIni[2]), np.squeeze(xEnd[2])],
+            strStyle, color=lColor, linewidth=lWidth)
+
+def drawRefSystem(ax, T_w_c, strStyle, nameStr):
+    """
+        Draw a reference system in a 3D plot: Red for X axis, Green for Y axis, and Blue for Z axis
+    -input:
+        ax: axis handle
+        T_w_c: (4x4 matrix) Reference system C seen from W.
+        strStyle: lines style.
+        nameStr: Name of the reference system.
+    """
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 0:1], strStyle, 'r', 1)
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 1:2], strStyle, 'g', 1)
+    draw3DLine(ax, T_w_c[0:3, 3:4], T_w_c[0:3, 3:4] + T_w_c[0:3, 2:3], strStyle, 'b', 1)
+    ax.text(np.squeeze( T_w_c[0, 3]+0.1), np.squeeze( T_w_c[1, 3]+0.1), np.squeeze( T_w_c[2, 3]+0.1), nameStr)
+
 # Forward Kannala model
 def projectionModel(X, K_c, D):
     phi = np.arctan2(X[1], X[0]);
@@ -112,12 +140,109 @@ if __name__ == '__main__':
     v_3_c2 = unprojectionModel(u_3_c2, K_2, D2_k_array);
     
     #-------------------------- Ex 2.2: triangulation --------------------------#
+    # Toy example
     X_w_true = T_wc1 @ X_1_c1 
     X_w_true = X_w_true / X_w_true[3];
     X_w = triangulatePoint(v_1_c1, v_1_c2, T_c1w, T_c2w);
     
+    # Full set of 3d points x1Data and x2Data
+    X_poseA_w = [];
+    for i in range(x1Data.shape[1]):
+        u_c1 = x1Data[:, i];
+        u_c2 = x2Data[:, i];
+        
+        v_c1 = unprojectionModel(u_c1, K_1, D1_k_array);
+        v_c2 = unprojectionModel(u_c2, K_2, D2_k_array);
+        
+        x_poseA_w = triangulatePoint(v_c1, v_c2, T_c1w, T_c2w);
+        X_poseA_w.append(x_poseA_w);
+    X_poseA_w = np.array(X_poseA_w).T;
     
-    #-------------------------- TEST --------------------------#
+    # Draw 2d matches
+    plt.figure(0)
+    plt.imshow(img1, cmap='gray', vmin=0, vmax=255)
+    plt.plot(x1Data[0, :], x1Data[1, :], 'rx')
+    plt.title('Image 1')
+    plt.draw()
+
+    plt.figure(1)
+    plt.imshow(img3, cmap='gray', vmin=0, vmax=255)
+    plt.plot(x2Data[0, :], x2Data[1, :], 'rx')
+    plt.title('Image 2')
+    plt.draw()
+    print('Close the figure to continue. Left button for orbit, right button for zoom.')
+    plt.show()
+    
+    # Draw 3D
+    ax = plt.axes(projection='3d', adjustable='box')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    drawRefSystem(ax, np.eye(4, 4), '-', 'W')
+    drawRefSystem(ax, T_wc1, '-', 'C1')
+    drawRefSystem(ax, T_wc2, '-', 'C2')
+    
+    ax.scatter(X_poseA_w[0, :], X_poseA_w[1, :], X_poseA_w[2, :], marker='.', c="green")
+    
+    #Matplotlib does not correctly manage the axis('equal')
+    xFakeBoundingBox = np.linspace(0, 4, 2)
+    yFakeBoundingBox = np.linspace(0, 4, 2)
+    zFakeBoundingBox = np.linspace(0, 4, 2)
+    plt.plot(xFakeBoundingBox, yFakeBoundingBox, zFakeBoundingBox, 'w.')
+    print('Close the figure to continue. Left button for orbit, right button for zoom.')
+    plt.show()
+        
+    
+    # Full set of 3d points x3Data and x4Data
+    X_poseB_w = [];
+    for i in range(x3Data.shape[1]):
+        u_c1 = x3Data[:, i];
+        u_c2 = x4Data[:, i];
+        
+        v_c1 = unprojectionModel(u_c1, K_1, D1_k_array);
+        v_c2 = unprojectionModel(u_c2, K_2, D2_k_array);
+        
+        x_poseB_w = triangulatePoint(v_c1, v_c2, T_c1w, T_c2w);
+        X_poseB_w.append(x_poseB_w);
+    X_poseB_w = np.array(X_poseB_w).T;
+    
+     # Draw 2d matches
+    plt.figure(2)
+    plt.imshow(img2, cmap='gray', vmin=0, vmax=255)
+    plt.plot(x3Data[0, :], x3Data[1, :], 'rx')
+    plt.title('Image 3')
+    plt.draw()
+
+    plt.figure(3)
+    plt.imshow(img4, cmap='gray', vmin=0, vmax=255)
+    plt.plot(x4Data[0, :], x4Data[1, :], 'rx')
+    plt.title('Image 4')
+    plt.draw()
+    print('Close the figure to continue. Left button for orbit, right button for zoom.')
+    plt.show()
+    
+    # Draw 3D
+    # Draw 3D
+    ax = plt.axes(projection='3d', adjustable='box')
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    drawRefSystem(ax, np.eye(4, 4), '-', 'W')
+    drawRefSystem(ax, T_wc1, '-', 'C1')
+    drawRefSystem(ax, T_wc2, '-', 'C2')
+    
+    ax.scatter(X_poseB_w[0, :], X_poseB_w[1, :], X_poseB_w[2, :], marker='.', c="green")
+    
+    #Matplotlib does not correctly manage the axis('equal')
+    xFakeBoundingBox = np.linspace(0, 4, 2)
+    yFakeBoundingBox = np.linspace(0, 4, 2)
+    zFakeBoundingBox = np.linspace(0, 4, 2)
+    plt.plot(xFakeBoundingBox, yFakeBoundingBox, zFakeBoundingBox, 'w.')
+    print('Close the figure to continue. Left button for orbit, right button for zoom.')
+    plt.show()
+    
+    
+    #-------------------------- CLASS EXAMPLE --------------------------#
     X_test_c1 = np.array([4, 0 ,10, 1]);
     X_test_c2 = np.array([-8, 0 ,10, 1]);
     v_test_c1 = np.array([4, 0, 10]);
