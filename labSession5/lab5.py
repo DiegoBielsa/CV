f46d6bac55a3f0d1d574a29fb9be20171615f2ed
DiegoBielsa <776564@unizar.es>
2023-12-02 23:07:10 +0100

    Lab5: ended

--- labSession5/lab5.py
+++ labSession5/lab5.py
@@ -22,8 +22,8 @@ def projectionModel(X, K_c, D):
     return u;
     
 # Backward Kannala model return x and v
-def unproectionModel(u, K_c, D):
-    x = np.linalg.inv(K_c) @ u;
+def unprojectionModel(u, K_c, D):
+    x = np.linalg.inv(K_c) @ u.T;
     phi = np.arctan2(x[1], x[0]);
     d = np.sqrt((x[0]**2 + x[1]**2) / x[2]**2);
     roots = np.roots([D[3], 0, D[2], 0, D[1], 0, D[0], 0, 1, -d]);
@@ -44,16 +44,13 @@ def triangulatePoint(v_1, v_2, T_c1w, T_c2w):
     pi_sym_2_w = T_c2w.T @ pi_sym_2_2;
     pi_per_2_w = T_c2w.T @ pi_per_2_2;
     
-    A = np.array([pi_sym_1_w.T, pi_per_1_w.T, pi_sym_2_w.T, pi_per_2_w.T]).T
+    A = np.array([pi_sym_1_w.T, pi_per_1_w.T, pi_sym_2_w.T, pi_per_2_w.T])
     
     _, _, vt = np.linalg.svd(A);
     X_w = vt[-1, :];
     X_w = X_w / X_w[3];
     
     # X_w is in world ref, be careful so the X_1 is given in cam1 ref
-    
-    """X_w = T_c1w @ X_w 
-    X_w = X_w / X_w[3];"""
     
     return X_w;
     
@@ -78,6 +75,7 @@ if __name__ == '__main__':
     
     T_c1w = np.linalg.inv(T_wc1);
     T_c2w = np.linalg.inv(T_wc2);
+    T_c2_c1 = T_c2w @ T_wc1;
     
     ## 2D plotting example
     img1 = cv2.cvtColor(cv2.imread('fisheye1_frameA.png'), cv2.COLOR_BGR2RGB)
@@ -87,29 +85,62 @@ if __name__ == '__main__':
     
     ############################ EXERCISE 2 ############################
     #-------------------------- Ex 2.1: projection --------------------------#
-    X_1 = np.array([3, 2, 10, 1]).T;
-    X_2 = np.array([-5, 6, 7, 1]).T;
-    X_3 = np.array([1, 5, 14, 1]).T;
+    X_1_c1 = np.array([3, 2, 10, 1]).T;
+    X_2_c1 = np.array([-5, 6, 7, 1]).T;
+    X_3_c1 = np.array([1, 5, 14, 1]).T;
     
-    u_1_c1 = projectionModel(X_1, K_1, D1_k_array);
-    u_2_c1 = projectionModel(X_2, K_1, D1_k_array);
-    u_3_c1 = projectionModel(X_3, K_1, D1_k_array);
+    u_1_c1 = projectionModel(X_1_c1, K_1, D1_k_array);
+    u_2_c1 = projectionModel(X_2_c1, K_1, D1_k_array);
+    u_3_c1 = projectionModel(X_3_c1, K_1, D1_k_array);
     
     #move point to cam2
-    u_1_c2 = projectionModel(X_1, K_2, D2_k_array);
-    u_2_c2 = projectionModel(X_2, K_2, D2_k_array);
-    u_3_c2 = projectionModel(X_3, K_2, D2_k_array);
+    X_1_c2 = T_c2_c1 @ X_1_c1;
+    X_2_c2 = T_c2_c1 @ X_2_c1;
+    X_3_c2 = T_c2_c1 @ X_3_c1;
+    
+    u_1_c2 = projectionModel(X_1_c2, K_2, D2_k_array);
+    u_2_c2 = projectionModel(X_2_c2, K_2, D2_k_array);
+    u_3_c2 = projectionModel(X_3_c2, K_2, D2_k_array);
     
     #-------------------------- Ex 2.1: unprojection --------------------------#
-    v_1_c1 = unproectionModel(u_1_c1, K_1, D1_k_array);
-    v_2_c1 = unproectionModel(u_2_c1, K_1, D1_k_array);
-    v_3_c1 = unproectionModel(u_3_c1, K_1, D1_k_array);
+    v_1_c1 = unprojectionModel(u_1_c1, K_1, D1_k_array);
+    v_2_c1 = unprojectionModel(u_2_c1, K_1, D1_k_array);
+    v_3_c1 = unprojectionModel(u_3_c1, K_1, D1_k_array);
     
-    v_1_c2 = unproectionModel(u_1_c2, K_2, D2_k_array);
-    v_2_c2 = unproectionModel(u_2_c2, K_2, D2_k_array);
-    v_3_c2 = unproectionModel(u_3_c2, K_2, D2_k_array);
+    v_1_c2 = unprojectionModel(u_1_c2, K_2, D2_k_array);
+    v_2_c2 = unprojectionModel(u_2_c2, K_2, D2_k_array);
+    v_3_c2 = unprojectionModel(u_3_c2, K_2, D2_k_array);
     
     #-------------------------- Ex 2.2: triangulation --------------------------#
-    X_w = triangulatePoint(v_1_c1, v_1_c2, T_c1w, T_c2w)
+    X_w_true = T_wc1 @ X_1_c1 
+    X_w_true = X_w_true / X_w_true[3];
+    X_w = triangulatePoint(v_1_c1, v_1_c2, T_c1w, T_c2w);
+    
+    
+    #-------------------------- TEST --------------------------#
+    X_test_c1 = np.array([4, 0 ,10, 1]);
+    X_test_c2 = np.array([-8, 0 ,10, 1]);
+    v_test_c1 = np.array([4, 0, 10]);
+    v_test_c2 = np.array([-8, 0, 10]);
+    
+    T_test_c1c2 = np.array([[1, 0, 0, 12],
+                           [0, 1, 0, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]]);
+    T_test_wc1 = np.array([[1, 0, 0, -6],
+                           [0, 1, 0, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]]);
+    T_test_wc2 = np.array([[1, 0, 0, 6],
+                           [0, 1, 0, 0],
+                           [0, 0, 1, 0],
+                           [0, 0, 0, 1]]);
+    
+    T_test_c1w = np.linalg.inv(T_test_wc1);
+    T_test_c2w = np.linalg.inv(T_test_wc2);
+    
+    X_w = triangulatePoint(v_test_c1, v_test_c2, T_test_c1w, T_test_c2w)
+    X_w_test = T_test_wc1 @ X_test_c1;
+    X_w_test = T_test_wc2 @ X_test_c2;
     
     a = 0;
